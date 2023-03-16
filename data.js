8a6cd3669bfa4710b9517bc1603662b9e61d46ee
Viktor <vik9269@mail.ru>
2023-03-16 18:56:10 +0700

    finatJS

---
+++ data.js
@@ -0,0 +1,74 @@
+const dataFeturedItems = `[
+    {
+        "image": "img/foto_1.jpg",
+        "name": "КУРТКА (мужицкая)",
+        "description": "Простая синяя куртская, уважать в ней вас больше не станут, от того и цена...",
+        "price": "$22.00"
+    },
+    {
+        "image": "img/foto_2.png",
+        "name": "КОСТЮМЧИК (женский)",
+        "description": "Прикупи такой своей бывшей, пускай знаекакого парнишку упустила! Преподносить с чеком!",
+        "price": "$2000.00"
+    },
+    {
+        "image": "img/foto_3.png",
+        "name": "НАБОР ПЛЯЖНЫЙ ДНЕВНОЙ (мужской)",
+        "description": "Сибирские пляжи суровы, почти как Федя Емельяненко в лучшие годы.",
+        "price": "$49.99"
+    },
+    {
+        "image": "img/foto_4.png",
+        "name": "ШТАНЦЫ С ГРЯЗНЫМИ КАРМАНЦАМИ",
+        "description": "Что лежит в ваших грязных карманцах, мистер Бэггинс???",
+        "price": "$35.00"
+    },
+    {
+        "image": "img/foto_5.png",
+        "name": "ПИДЖАЧОК (дамский)",
+        "description": "Простой голубой пиджачелло, что тут еще описывать, я х.з",
+        "price": "$78.00"
+    },
+    {
+        "image": "img/foto_6.png",
+        "name": "УВЕЛИЧИТЕЛЬНОЕ СТЕКЛО - ПАЛЬЧИКИ",
+        "description": "Это Даша! Помоги Даше построить свою любовь в ДОМ 2. Ну не в моготу уже...",
+        "price": "$100.00"
+    },
+    {
+        "image": "img/foto_1.jpg",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    },
+    {
+        "image": "img/foto_2.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    },
+    {
+        "image": "img/foto_3.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    },
+    {
+        "image": "img/foto_4.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    },
+    {
+        "image": "img/foto_5.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    },
+    {
+        "image": "img/foto_6.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi",
+        "price": "$52.00"
+    }
+    ]`
